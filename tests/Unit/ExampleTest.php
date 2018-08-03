97c0eb39f6d77aacb6e8ce994269a1657c5db864
tranvandong96 <superhihiha@gmail.com>
2018-08-03 16:40:38 +0700

    22. Testing 101

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,11 +2,15 @@
 
 namespace Tests\Unit;
 
+use App\Post;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
+    use DatabaseTransactions;
+
     /**
      * A basic test example.
      *
@@ -14,6 +18,25 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+        $first = factory(Post::class)->create();
+        $second = factory(Post::class)->create([
+            'created_at' => \Carbon\Carbon::now()->subMonth()
+        ]);
+
+        $posts = Post::archives();
+
+        $this->assertEquals([
+            [
+                "year" => $first->created_at->format('Y'),
+                "month" => $first->created_at->format('F'),
+                "published" => 1
+            ],
+            [
+                "year" => $second->created_at->format('Y'),
+                "month" => $second->created_at->format('F'),
+                "published" => 1
+            ]
+
+        ], $posts);
     }
 }
